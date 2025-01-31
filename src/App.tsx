a2f838bc8f6b47161f34414598342ffbcc08a206
Mchumbles <lewis.mcguire92@yahoo.com>
2025-01-31 14:36:20 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <body className="bg-slate-900">
+      <h1 className="text-center text-3xl text-white p-4  ">Lewis Mcguire</h1>
+      <h2 className="text-center text-lg text-white p-2">
+        Junior Software Engineer & Web Developer
+      </h2>
+      <div className="flex flex-col items-center">
+        <button className="bg-cyan-600 shadow-lg shadow-cyan-600/50 hover:bg-cyan-900 text-white font-bold py-2 px-4 rounded">
+          <a href="/Lewis-Mcguire-CV-2025.pdf" target="_blank">
+            Curriculum Vitae
+          </a>
+        </button>
+      </div>
+    </body>
+  );
+}
+
+export default App;
